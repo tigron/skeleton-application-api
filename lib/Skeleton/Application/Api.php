14606aee4b7f4769cfeca16d4fb10a5c9b7fc728
Christophe Gosiau <christophe.gosiau@tigron.be>
2021-04-13 18:26:16 +0200

    first commit

---
+++ lib/Skeleton/Application/Api.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Skeleton Core Application class
+ *
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author Gerry Demaret <gerry@tigron.be>
+ */
+
+namespace Skeleton\Core\Application;
+
+class Api extends \Skeleton\Core\Application {
+
+	/**
+	 * Load the config
+	 *
+	 * @access private
+	 */
+	protected function load_config() {
+		parent::load_config();
+	}
+
+	/**
+	 * Run the application
+	 *
+	 * @access public
+	 */
+	public function run() {
+	}
+
+}
