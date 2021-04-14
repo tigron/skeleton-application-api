2c53930f322178a379eb5ff5cb640b0911679e67
Christophe Gosiau <christophe.gosiau@tigron.be>
2021-04-15 00:50:59 +0200

    work in progress

--- lib/Skeleton/Application/Api.php
+++ lib/Skeleton/Application/Api.php
@@ -6,9 +6,32 @@
  * @author Gerry Demaret <gerry@tigron.be>
  */
 
-namespace Skeleton\Core\Application;
+namespace Skeleton\Application;
 
 class Api extends \Skeleton\Core\Application {
+
+	/**
+	 * Module Path
+	 *
+	 * @var string $module_path
+	 * @access public
+	 */
+	public $endpoint_path = null;
+
+	/**
+	 * Get details
+	 *
+	 * @access protected
+	 */
+	protected function get_details() {
+		parent::get_details();
+		
+		$this->endpoint_path = $this->path . '/endpoint/';
+
+		$autoloader = new \Skeleton\Core\Autoloader();
+		$autoloader->add_namespace("\\App\\" . $this->name . "\Endpoint\\", $this->endpoint_path);
+		$autoloader->register();
+	}	
 
 	/**
 	 * Load the config
@@ -25,6 +48,44 @@ class Api extends \Skeleton\Core\Application {
 	 * @access public
 	 */
 	public function run() {
+		$request = pathinfo($this->request_relative_uri);
+		if (isset($request['extension']) and $request['extension'] == 'json') {
+			$this->run_openapi();
+		}
 	}
+	
+	public function run_openapi() {
+		$endpoints = $this->get_endpoints();
+		foreach ($endpoints as $endpoint) {
+			print_r($endpoint->get_paths());
+		}
+	}
+
+	/**
+	 * Get endpoints
+	 *
+	 * @access public
+	 */
+	public function get_endpoints() {
+		$iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->endpoint_path));
+		$files = array_filter(iterator_to_array($iterator), function($file) {
+			return $file->isFile();
+		});
+		$endpoints = [];
+
+		foreach ($files as $file) {
+			if ($file->getExtension() != 'php') {
+				continue;
+			}
+			$pathinfo = pathinfo($file->getPathname());
+			$endpoint = str_replace($this->endpoint_path, '', $pathinfo['dirname'] . DIRECTORY_SEPARATOR) . $pathinfo['filename'];
+			$endpoint = str_replace(DIRECTORY_SEPARATOR, '\\', $endpoint);
+
+			$classname = "\\App\\" . ucfirst($this->name) . "\Endpoint\\" . $endpoint;
+			$class = new $classname();
+			$endpoints[] = $class;
+		}
+		return $endpoints;
+	}	
 
 }
